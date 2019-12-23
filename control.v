1f4be5f09c8742ca89029931463fcfbf4e90ffb4
LEE-JAE-HYUN179 <leejaehyun1223@gmail.com>
2019-12-23 17:48:20 +0900

    lee jae hyun

--- control.v
+++ control.v
@@ -1,32 +1,42 @@
-module control(inst,RegWrite,RegDst,AluSrc,Branch,MemWrite,MemRead,MemtoReg,AluOp,jump);
+module control(inst,func,RegWrite,RegDst,AluSrc,Branch,MemWrite,MemRead,MemtoReg,AluOp,jump);
 
 input [5:0] inst;
+input [5:0] func;
 output RegWrite,RegDst,AluSrc,Branch,MemWrite,MemRead,MemtoReg,jump;
-output [1:0] AluOp;
-reg [9:0] out;
+output [3:0] AluOp;
+reg [11:0] out;
 
-always@(inst)begin
-    case (inst)
-        6'b000000: out<=10'b1100000100;//r
-        6'b100011: out<=10'b1010011000;//lw
-        6'b101011: out<=10'b0010100000;//sw
-        6'b000100: out<=10'b0001000010;//beq
-        6'b001000: out<=10'b1010000000;//addi
-        6'b001101: out<=10'b1010000100;//ori
-        6'b000010: out<=10'b0000000001;//j
-        default: out<=10'b0000000000;
-    endcase
+always@(inst,func)begin
+    if(inst==6'b000000)begin //r
+        if(func==6'b100000) out<=12'b110001000100;//add
+        else if(func==6'b000000)out<=12'b10000100010100;//sll
+        else if(func==6'b000010)out<=12'b11000100011000;//srl
+        else if(func==6'b100010)out<=12'b11000100001100;//sub
+        else if(func==6'b100100)out<=12'b11000100000000;//and
+        else if(func==6'b100101)out<=12'b11000100000010;//or
+        else if(func==6'b101011)out<=12'b11000100001110;//sltu
+        
+
+    end
+    else if(inst== 6'b100011) out<=12'b101001100100;//lw
+    else if(inst==6'b101011) out<=12'b001010000100;//sw
+    else if(inst==6'b000100) out<=12'b000100001100;//beq
+    else if (inst==6'b001000) out<=12'b101000000100;//addi
+    else if(inst==6'b001101) out<=12'b101000000010;//ori
+    else if(inst==6'b000010) out<=12'b000000000101;//j
+    else out<=12'b000000000000;
+    
 
 end
 
-assign RegWrite=out[9];
-assign RegDst=out[8];
-assign AluSrc=out[7];
-assign Branch=out[6];
-assign MemWrite=out[5];
-assign MemRead=out[4];
-assign MemtoReg=out[3];
-assign AluOp=out[2:1];
+assign RegWrite=out[11];
+assign RegDst=out[10];
+assign AluSrc=out[9];
+assign Branch=out[8];
+assign MemWrite=out[7];
+assign MemRead=out[6];
+assign MemtoReg=out[5];
+assign AluOp=out[4:1];
 assign jump=out[0];
 
 endmodule
--- mips_cpu.v
+++ mips_cpu.v
@@ -9,21 +9,21 @@ module mips_cpu(rst,clk,out_pc,out_result);
     wire [31:0] new_pc,now_pc,next_pc,branced_pc,jump_pc,branch_selected;
     wire alu_zero,is_branced;
     wire RegWrite,RegDst,AluSrc,Branch,MemWrite,MemRead,MemtoReg,jump;
-    wire [1:0] AluOp;
-    wire [3:0] Alu_conrtol;
+    wire [3:0] AluOp;
+    
     
 
 
     pc p1(rst,clk,new_pc,now_pc);
     jump_concat jcon(instruction[25:0],next_pc[31:28],jump_pc);
-    control c1(instruction[31:26],RegWrite,RegDst,AluSrc,Branch,MemWrite,MemRead,MemtoReg,AluOp,jump);
+    control c1(instruction[31:26],instruction[5:0],RegWrite,RegDst,AluSrc,Branch,MemWrite,MemRead,MemtoReg,AluOp,jump);
     instruction_mem ins_mem(now_pc,instruction);
     mux_5 regmux(instruction[20:16],instruction[15:11],RegDst,reg_wrtadr);
     register_mips r1(RegWrite,instruction[25:21],instruction[20:16],reg_wrtadr,reg_wrtdata,read_data1,read_data2);
     sign_extend se(instruction[15:0],sign_extended);
     mux_32 alscr(read_data2,sign_extended,AluSrc,alu_scr2);
-    aludec alcon(instruction[5:0],AluOp,Alu_conrtol);
-    alu_mips ALU(read_data1,alu_scr2,instruction[10:6],Alu_conrtol,alu_result,alu_zero);
+    
+    alu_mips ALU(read_data1,alu_scr2,instruction[10:6],AluOp,alu_result,alu_zero);
     data_mem dm(MemRead,MemWrite,alu_result,read_data2,mem_readdata);
     mux_32 memmux(alu_result,mem_readdata,MemtoReg,reg_wrtdata);
     mux_32 bmux(next_pc,branced_pc,is_branced,branch_selected);
